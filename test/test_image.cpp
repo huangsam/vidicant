4a1547c6b68aca07a3d3e4bb6de7061d062596eb
Samuel Huang <samhuang91@gmail.com>
2026-02-04 08:14:13 +0000

    Reformat the test code

--- test/test_image.cpp
+++ test/test_image.cpp
@@ -1,117 +1,127 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include <opencv2/opencv.hpp>
 #include "vidicant/image.hpp"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
 
 class MockImageLoader : public IImageLoader {
 public:
-    MOCK_METHOD(cv::Mat, imread, (const std::string&), (override));
+  MOCK_METHOD(cv::Mat, imread, (const std::string &), (override));
 };
 
 TEST(ImageHandlerTest, GetDimensionsSuccess) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat fakeImage(100, 200, CV_8UC3, cv::Scalar(0, 0, 0)); // height 100, width 200
-    EXPECT_CALL(*mockLoader, imread("test.jpg")).WillOnce(::testing::Return(fakeImage));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat fakeImage(100, 200, CV_8UC3,
+                    cv::Scalar(0, 0, 0)); // height 100, width 200
+  EXPECT_CALL(*mockLoader, imread("test.jpg"))
+      .WillOnce(::testing::Return(fakeImage));
 
-    ImageHandler handler(std::move(mockLoader));
-    auto [width, height] = handler.getDimensions("test.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  auto [width, height] = handler.getDimensions("test.jpg");
 
-    EXPECT_EQ(width, 200);
-    EXPECT_EQ(height, 100);
+  EXPECT_EQ(width, 200);
+  EXPECT_EQ(height, 100);
 }
 
 TEST(ImageHandlerTest, GetDimensionsFail) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat emptyImage;
-    EXPECT_CALL(*mockLoader, imread("bad.jpg")).WillOnce(::testing::Return(emptyImage));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat emptyImage;
+  EXPECT_CALL(*mockLoader, imread("bad.jpg"))
+      .WillOnce(::testing::Return(emptyImage));
 
-    ImageHandler handler(std::move(mockLoader));
-    auto [width, height] = handler.getDimensions("bad.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  auto [width, height] = handler.getDimensions("bad.jpg");
 
-    EXPECT_EQ(width, -1);
-    EXPECT_EQ(height, -1);
+  EXPECT_EQ(width, -1);
+  EXPECT_EQ(height, -1);
 }
 
 TEST(ImageHandlerTest, IsGrayscaleTrue) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat grayImage(10, 10, CV_8UC1, cv::Scalar(128));
-    EXPECT_CALL(*mockLoader, imread("gray.jpg")).WillOnce(::testing::Return(grayImage));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat grayImage(10, 10, CV_8UC1, cv::Scalar(128));
+  EXPECT_CALL(*mockLoader, imread("gray.jpg"))
+      .WillOnce(::testing::Return(grayImage));
 
-    ImageHandler handler(std::move(mockLoader));
-    bool isGray = handler.isGrayscale("gray.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  bool isGray = handler.isGrayscale("gray.jpg");
 
-    EXPECT_TRUE(isGray);
+  EXPECT_TRUE(isGray);
 }
 
 TEST(ImageHandlerTest, IsGrayscaleFalse) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat colorImage(10, 10, CV_8UC3, cv::Scalar(128, 128, 128));
-    EXPECT_CALL(*mockLoader, imread("color.jpg")).WillOnce(::testing::Return(colorImage));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat colorImage(10, 10, CV_8UC3, cv::Scalar(128, 128, 128));
+  EXPECT_CALL(*mockLoader, imread("color.jpg"))
+      .WillOnce(::testing::Return(colorImage));
 
-    ImageHandler handler(std::move(mockLoader));
-    bool isGray = handler.isGrayscale("color.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  bool isGray = handler.isGrayscale("color.jpg");
 
-    EXPECT_FALSE(isGray);
+  EXPECT_FALSE(isGray);
 }
 
 TEST(ImageHandlerTest, GetAverageBrightness) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat image(2, 2, CV_8UC3, cv::Scalar(100, 150, 200));
-    EXPECT_CALL(*mockLoader, imread("bright.jpg")).WillOnce(::testing::Return(image));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat image(2, 2, CV_8UC3, cv::Scalar(100, 150, 200));
+  EXPECT_CALL(*mockLoader, imread("bright.jpg"))
+      .WillOnce(::testing::Return(image));
 
-    ImageHandler handler(std::move(mockLoader));
-    double brightness = handler.getAverageBrightness("bright.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  double brightness = handler.getAverageBrightness("bright.jpg");
 
-    EXPECT_NEAR(brightness, 150.0, 1.0);  // (100+150+200)/3 = 150
+  EXPECT_NEAR(brightness, 150.0, 1.0); // (100+150+200)/3 = 150
 }
 
 TEST(ImageHandlerTest, GetNumberOfChannels) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat image(10, 10, CV_8UC3);
-    EXPECT_CALL(*mockLoader, imread("channels.jpg")).WillOnce(::testing::Return(image));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat image(10, 10, CV_8UC3);
+  EXPECT_CALL(*mockLoader, imread("channels.jpg"))
+      .WillOnce(::testing::Return(image));
 
-    ImageHandler handler(std::move(mockLoader));
-    int channels = handler.getNumberOfChannels("channels.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  int channels = handler.getNumberOfChannels("channels.jpg");
 
-    EXPECT_EQ(channels, 3);
+  EXPECT_EQ(channels, 3);
 }
 
 TEST(ImageHandlerTest, GetEdgeCount) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat image(10, 10, CV_8UC1, cv::Scalar(0));
-    // Add some edges by setting some pixels to 255
-    image.at<uchar>(5, 5) = 255;
-    EXPECT_CALL(*mockLoader, imread("edges.jpg")).WillOnce(::testing::Return(image));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat image(10, 10, CV_8UC1, cv::Scalar(0));
+  // Add some edges by setting some pixels to 255
+  image.at<uchar>(5, 5) = 255;
+  EXPECT_CALL(*mockLoader, imread("edges.jpg"))
+      .WillOnce(::testing::Return(image));
 
-    ImageHandler handler(std::move(mockLoader));
-    int edgeCount = handler.getEdgeCount("edges.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  int edgeCount = handler.getEdgeCount("edges.jpg");
 
-    EXPECT_GT(edgeCount, 0);  // Should detect some edges
+  EXPECT_GT(edgeCount, 0); // Should detect some edges
 }
 
 TEST(ImageHandlerTest, GetDominantColors) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat image(10, 10, CV_8UC3, cv::Scalar(255, 0, 0));  // Red image
-    EXPECT_CALL(*mockLoader, imread("colors.jpg")).WillOnce(::testing::Return(image));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat image(10, 10, CV_8UC3, cv::Scalar(255, 0, 0)); // Red image
+  EXPECT_CALL(*mockLoader, imread("colors.jpg"))
+      .WillOnce(::testing::Return(image));
 
-    ImageHandler handler(std::move(mockLoader));
-    auto colors = handler.getDominantColors("colors.jpg", 1);
+  ImageHandler handler(std::move(mockLoader));
+  auto colors = handler.getDominantColors("colors.jpg", 1);
 
-    EXPECT_EQ(colors.size(), 1);
-    EXPECT_NEAR(colors[0][0], 255.0, 10.0);  // Red channel
-    EXPECT_NEAR(colors[0][1], 0.0, 10.0);    // Green
-    EXPECT_NEAR(colors[0][2], 0.0, 10.0);    // Blue
+  EXPECT_EQ(colors.size(), 1);
+  EXPECT_NEAR(colors[0][0], 255.0, 10.0); // Red channel
+  EXPECT_NEAR(colors[0][1], 0.0, 10.0);   // Green
+  EXPECT_NEAR(colors[0][2], 0.0, 10.0);   // Blue
 }
 
 TEST(ImageHandlerTest, GetBlurScore) {
-    auto mockLoader = std::make_unique<MockImageLoader>();
-    cv::Mat sharpImage(10, 10, CV_8UC1, cv::Scalar(128));
-    // Make it sharp by adding high frequency
-    sharpImage.at<uchar>(5, 5) = 255;
-    EXPECT_CALL(*mockLoader, imread("sharp.jpg")).WillOnce(::testing::Return(sharpImage));
+  auto mockLoader = std::make_unique<MockImageLoader>();
+  cv::Mat sharpImage(10, 10, CV_8UC1, cv::Scalar(128));
+  // Make it sharp by adding high frequency
+  sharpImage.at<uchar>(5, 5) = 255;
+  EXPECT_CALL(*mockLoader, imread("sharp.jpg"))
+      .WillOnce(::testing::Return(sharpImage));
 
-    ImageHandler handler(std::move(mockLoader));
-    double blurScore = handler.getBlurScore("sharp.jpg");
+  ImageHandler handler(std::move(mockLoader));
+  double blurScore = handler.getBlurScore("sharp.jpg");
 
-    EXPECT_GT(blurScore, 0.0);  // Should have some variance
+  EXPECT_GT(blurScore, 0.0); // Should have some variance
 }
--- test/test_video.cpp
+++ test/test_video.cpp
@@ -1,137 +1,149 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include <opencv2/opencv.hpp>
 #include "vidicant/video.hpp"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
 
 class MockVideoLoader : public IVideoLoader {
 public:
-    MOCK_METHOD(bool, open, (const std::string&), (override));
-    MOCK_METHOD(int, getFrameCount, (), (override));
-    MOCK_METHOD(double, getFPS, (), (override));
-    MOCK_METHOD((std::pair<int, int>), getResolution, (), (override));
-    MOCK_METHOD(cv::Mat, readFrame, (), (override));
+  MOCK_METHOD(bool, open, (const std::string &), (override));
+  MOCK_METHOD(int, getFrameCount, (), (override));
+  MOCK_METHOD(double, getFPS, (), (override));
+  MOCK_METHOD((std::pair<int, int>), getResolution, (), (override));
+  MOCK_METHOD(cv::Mat, readFrame, (), (override));
 };
 
 TEST(VideoHandlerTest, GetFrameCount) {
-    auto mockLoader = std::make_unique<MockVideoLoader>();
-    EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
-    EXPECT_CALL(*mockLoader, getFrameCount()).WillOnce(::testing::Return(100));
+  auto mockLoader = std::make_unique<MockVideoLoader>();
+  EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
+  EXPECT_CALL(*mockLoader, getFrameCount()).WillOnce(::testing::Return(100));
 
-    VideoHandler handler(std::move(mockLoader));
-    handler.open("test.mp4");
-    int frameCount = handler.getFrameCount();
+  VideoHandler handler(std::move(mockLoader));
+  handler.open("test.mp4");
+  int frameCount = handler.getFrameCount();
 
-    EXPECT_EQ(frameCount, 100);
+  EXPECT_EQ(frameCount, 100);
 }
 
 TEST(VideoHandlerTest, GetFPS) {
-    auto mockLoader = std::make_unique<MockVideoLoader>();
-    EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
-    EXPECT_CALL(*mockLoader, getFPS()).WillOnce(::testing::Return(30.0));
+  auto mockLoader = std::make_unique<MockVideoLoader>();
+  EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
+  EXPECT_CALL(*mockLoader, getFPS()).WillOnce(::testing::Return(30.0));
 
-    VideoHandler handler(std::move(mockLoader));
-    handler.open("test.mp4");
-    double fps = handler.getFPS();
+  VideoHandler handler(std::move(mockLoader));
+  handler.open("test.mp4");
+  double fps = handler.getFPS();
 
-    EXPECT_EQ(fps, 30.0);
+  EXPECT_EQ(fps, 30.0);
 }
 
 TEST(VideoHandlerTest, GetResolution) {
-    auto mockLoader = std::make_unique<MockVideoLoader>();
-    EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
-    EXPECT_CALL(*mockLoader, getResolution()).WillOnce(::testing::Return(std::make_pair(1920, 1080)));
+  auto mockLoader = std::make_unique<MockVideoLoader>();
+  EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
+  EXPECT_CALL(*mockLoader, getResolution())
+      .WillOnce(::testing::Return(std::make_pair(1920, 1080)));
 
-    VideoHandler handler(std::move(mockLoader));
-    handler.open("test.mp4");
-    auto [width, height] = handler.getResolution();
+  VideoHandler handler(std::move(mockLoader));
+  handler.open("test.mp4");
+  auto [width, height] = handler.getResolution();
 
-    EXPECT_EQ(width, 1920);
-    EXPECT_EQ(height, 1080);
+  EXPECT_EQ(width, 1920);
+  EXPECT_EQ(height, 1080);
 }
 
 TEST(VideoHandlerTest, GetDuration) {
-    auto mockLoader = std::make_unique<MockVideoLoader>();
-    EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
-    EXPECT_CALL(*mockLoader, getFrameCount()).WillOnce(::testing::Return(300));
-    EXPECT_CALL(*mockLoader, getFPS()).WillOnce(::testing::Return(30.0));
+  auto mockLoader = std::make_unique<MockVideoLoader>();
+  EXPECT_CALL(*mockLoader, open("test.mp4")).WillOnce(::testing::Return(true));
+  EXPECT_CALL(*mockLoader, getFrameCount()).WillOnce(::testing::Return(300));
+  EXPECT_CALL(*mockLoader, getFPS()).WillOnce(::testing::Return(30.0));
 
-    VideoHandler handler(std::move(mockLoader));
-    handler.open("test.mp4");
-    double duration = handler.getDuration();
+  VideoHandler handler(std::move(mockLoader));
+  handler.open("test.mp4");
+  double duration = handler.getDuration();
 
-    EXPECT_EQ(duration, 10.0);  // 300 / 30
+  EXPECT_EQ(duration, 10.0); // 300 / 30
 }
 
 TEST(VideoHandlerTest, OpenFail) {
-    auto mockLoader = std::make_unique<MockVideoLoader>();
-    EXPECT_CALL(*mockLoader, open("bad.mp4")).WillOnce(::testing::Return(false));
+  auto mockLoader = std::make_unique<MockVideoLoader>();
+  EXPECT_CALL(*mockLoader, open("bad.mp4")).WillOnce(::testing::Return(false));
 
-    VideoHandler handler(std::move(mockLoader));
-    bool opened = handler.open("bad.mp4");
+  VideoHandler handler(std::move(mockLoader));
+  bool opened = handler.open("bad.mp4");
 
-    EXPECT_FALSE(opened);
+  EXPECT_FALSE(opened);
 }
 
 // Tests using real files for methods that need frame reading
 TEST(VideoGlobalTest, GetVideoFrameCountReal) {
-    int frameCount = vidicant::getVideoFrameCount("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_EQ(frameCount, 250);
+  int frameCount =
+      vidicant::getVideoFrameCount("/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_EQ(frameCount, 250);
 }
 
 TEST(VideoGlobalTest, GetVideoFPSReal) {
-    double fps = vidicant::getVideoFPS("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_EQ(fps, 25.0);
+  double fps =
+      vidicant::getVideoFPS("/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_EQ(fps, 25.0);
 }
 
 TEST(VideoGlobalTest, GetVideoResolutionReal) {
-    auto [width, height] = vidicant::getVideoResolution("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_EQ(width, 320);
-    EXPECT_EQ(height, 176);
+  auto [width, height] =
+      vidicant::getVideoResolution("/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_EQ(width, 320);
+  EXPECT_EQ(height, 176);
 }
 
 TEST(VideoGlobalTest, GetVideoDurationReal) {
-    double duration = vidicant::getVideoDuration("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_EQ(duration, 10.0);
+  double duration =
+      vidicant::getVideoDuration("/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_EQ(duration, 10.0);
 }
 
 TEST(VideoGlobalTest, ExtractFirstFrameReal) {
-    cv::Mat frame = vidicant::extractFirstFrame("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_FALSE(frame.empty());
-    EXPECT_EQ(frame.cols, 320);
-    EXPECT_EQ(frame.rows, 176);
+  cv::Mat frame =
+      vidicant::extractFirstFrame("/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_FALSE(frame.empty());
+  EXPECT_EQ(frame.cols, 320);
+  EXPECT_EQ(frame.rows, 176);
 }
 
 TEST(VideoGlobalTest, GetVideoAverageBrightnessReal) {
-    double brightness = vidicant::getVideoAverageBrightness("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_GT(brightness, 0.0);
+  double brightness = vidicant::getVideoAverageBrightness(
+      "/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_GT(brightness, 0.0);
 }
 
 TEST(VideoGlobalTest, IsVideoGrayscaleReal) {
-    bool grayscale = vidicant::isVideoGrayscale("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_FALSE(grayscale);
+  bool grayscale =
+      vidicant::isVideoGrayscale("/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_FALSE(grayscale);
 }
 
 TEST(VideoGlobalTest, SaveFirstFrameAsImageReal) {
-    bool saved = vidicant::saveFirstFrameAsImage("/workspaces/vidicant/examples/sample.mp4", "/workspaces/vidicant/examples/test_first_frame.jpg");
-    EXPECT_TRUE(saved);
-    // Clean up
-    std::remove("/workspaces/vidicant/examples/test_first_frame.jpg");
+  bool saved = vidicant::saveFirstFrameAsImage(
+      "/workspaces/vidicant/examples/sample.mp4",
+      "/workspaces/vidicant/examples/test_first_frame.jpg");
+  EXPECT_TRUE(saved);
+  // Clean up
+  std::remove("/workspaces/vidicant/examples/test_first_frame.jpg");
 }
 
 TEST(VideoGlobalTest, GetVideoMotionScoreReal) {
-    double motion = vidicant::getVideoMotionScore("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_GE(motion, 0.0);
+  double motion =
+      vidicant::getVideoMotionScore("/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_GE(motion, 0.0);
 }
 
 TEST(VideoGlobalTest, GetVideoDominantColorsReal) {
-    auto colors = vidicant::getVideoDominantColors("/workspaces/vidicant/examples/sample.mp4");
-    EXPECT_EQ(colors.size(), 3);
-    for (const auto& color : colors) {
-        EXPECT_GE(color[0], 0.0);
-        EXPECT_LE(color[0], 255.0);
-        EXPECT_GE(color[1], 0.0);
-        EXPECT_LE(color[1], 255.0);
-        EXPECT_GE(color[2], 0.0);
-        EXPECT_LE(color[2], 255.0);
-    }
+  auto colors = vidicant::getVideoDominantColors(
+      "/workspaces/vidicant/examples/sample.mp4");
+  EXPECT_EQ(colors.size(), 3);
+  for (const auto &color : colors) {
+    EXPECT_GE(color[0], 0.0);
+    EXPECT_LE(color[0], 255.0);
+    EXPECT_GE(color[1], 0.0);
+    EXPECT_LE(color[1], 255.0);
+    EXPECT_GE(color[2], 0.0);
+    EXPECT_LE(color[2], 255.0);
+  }
 }
