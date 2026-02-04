b1f83a9484ca2b27e0c29e0604d0e55399b8ddd9
Samuel Huang <samhuang91@gmail.com>
2026-02-04 07:32:35 +0000

    Remove hello module

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
-#include "vidicant/hello.hpp"
 #include "vidicant/image.hpp"
 #include <iostream>
 
 int main() {
-  // Instantiate your class using the vidicant namespace
-  const vidicant::Hello greeter("huangsam");
-
-  // Call the method
-  greeter.greet();
-
   // Get image dimensions
   auto [width, height] = vidicant::getImageDimensions("examples/sample.jpg");
   if (width == -1) {
